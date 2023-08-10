cfdeb9901c271a39b17d62132adeee5fc1d11cc4
Andrejs <127331717+Dreis27@users.noreply.github.com>
2023-08-10 20:07:27 +0200

    create ListItem class

---
+++ src/list-item.js
@@ -0,0 +1,19 @@
+class ListItem {
+    constructor(name, dueDate){
+        this.name = name;
+        this.dueDate = dueDate;
+    }
+
+    getName(){
+        return this.name;
+    }
+    getDate(){
+        return this.dueDate;
+    }
+    setName(name){
+        this.name=name;
+    }
+    setDate(dueDate){
+        this.dueDate=dueDate;
+    }
+}
